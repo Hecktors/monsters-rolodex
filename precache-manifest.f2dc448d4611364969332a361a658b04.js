6a116d387a1a12a615abf1ca7ba7e535ef9075b5
t <hectors.dev@gmail.com>
2020-01-20 13:18:49 -0400

    Updates

---
+++ precache-manifest.f2dc448d4611364969332a361a658b04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef12937d4d7158f34670f465cab99987",
+    "url": "/Hecktors/github.io/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "680214daca0ee71b8176",
+    "url": "/Hecktors/github.io/monsters-rolodex/static/css/main.b2a3df5a.chunk.css"
+  },
+  {
+    "revision": "c68e14fdc7e1ba4a5621",
+    "url": "/Hecktors/github.io/monsters-rolodex/static/js/2.9e7d0785.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Hecktors/github.io/monsters-rolodex/static/js/2.9e7d0785.chunk.js.LICENSE"
+  },
+  {
+    "revision": "680214daca0ee71b8176",
+    "url": "/Hecktors/github.io/monsters-rolodex/static/js/main.fe5fed82.chunk.js"
+  },
+  {
+    "revision": "a6b414e221f7223f5858",
+    "url": "/Hecktors/github.io/monsters-rolodex/static/js/runtime-main.0f51fd60.js"
+  }
+]);
