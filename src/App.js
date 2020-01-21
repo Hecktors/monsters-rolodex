16ed3953cd1d3e1beb1578155b89fb6867e60f6d
t <hectors.dev@gmail.com>
2020-01-21 12:00:38 -0400

    add mobile style, add favicons, add gh-pages

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { CardList } from './components/card-list/card-list.component'
 import { SearchBox } from './components/search-box/search-box.component'
+import { Scroll } from './components/scroll/scroll.component'
 
 class App extends React.Component {
   constructor() {
@@ -23,14 +24,14 @@ class App extends React.Component {
   }
 
   render() {
-    const { monsters, searchTerm} = this.state
+    const { monsters, searchTerm } = this.state
     const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchTerm.toLowerCase()))
 
     return (
       <div className="App">
         <h1>Monsters Rolodex</h1>
-        <SearchBox searchTerm={searchTerm} onChangeSearch={this.onChangeSearch} placeholder="Search the monsters"/>
-        <CardList monsters={filteredMonsters} />
+        <SearchBox searchTerm={searchTerm} onChangeSearch={this.onChangeSearch} placeholder="Search the monsters" />
+        <Scroll> <CardList monsters={filteredMonsters} /></Scroll>
       </div>
     );
   }
---
+++ src/components/scroll/scroll.component.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import './scroll.styles.css'
+
+export const Scroll = props => (
+    <div className="scroll">{props.children}</div>
+)
